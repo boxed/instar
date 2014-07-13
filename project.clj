0c2bb59fe5e76f1c2cae3a484e1fe75d63f179f5
Anders Hovmöller <boxed@killingar.net>
2014-07-13 22:31:24 +0200

    First release on clojars

--- project.clj
+++ project.clj
@@ -1,5 +1,41 @@
-(defproject instar "0.0.1-SNAPSHOT"
+(defproject instar "1.0.0"
   :description "transform nested data structures easily"
-  :dependencies [[org.clojure/clojure "1.5.1"]]
+  :dependencies [[org.clojure/clojure "1.5.1"]
+                 [org.clojure/clojurescript "0.0-2173"]]
+  :jar-exclusions [#"\.cljx|\.swp|\.swo|\.DS_Store"]
+  :source-paths ["src/cljx" "src/clj" "src/cljs"]
+  :test-paths ["target/test-classes"]
   :profiles {:dev {:dependencies [[midje "1.5.0"]]}}
-  :uberjar-name "instar.jar")
+  :plugins [[com.keminglabs/cljx "0.4.0"]
+            [lein-cljsbuild "1.0.2"]]
+  :uberjar-name "instar.jar"
+
+  :license "MIT"
+  :url "https://github.com/boxed/instar"
+
+  :hooks [cljx.hooks
+          leiningen.cljsbuild]
+
+  ; - clsj config -
+  :cljsbuild {:builds {:client {:source-paths ["target/classes"]
+                                :compiler {:output-dir "target/client"
+                                           :output-to "target/client.js"
+                                           ;:source-map "target/client.js.map"
+                                           :pretty-print true}}}}
+
+  ; - cljx config -
+  :hooks []
+
+  :cljx {:builds [{:source-paths ["src/cljx" "src/clj"]
+                   :output-path "target/classes"
+                   :rules :clj}
+                  {:source-paths ["src/cljx" "src/cljs"]
+                   :output-path "target/classes"
+                   :rules :cljs}
+
+                  {:source-paths ["test/cljx" "src/clj"]
+                   :output-path "target/test-classes"
+                   :rules :clj}
+                  {:source-paths ["test/cljx" "src/cljs"]
+                   :output-path "target/test-classes"
+                   :rules :cljs}]})
---
+++ src/cljs/instar/stack.cljs
@@ -0,0 +1,29 @@
+(ns instar.stack)
+
+(defn pop! [stack]
+  (.pop stack))
+
+(defn push! [stack value]
+  (.push stack value))
+
+(defn push-all! [stack value-list]
+  (doseq [x value-list]
+    (.push stack x)))
+
+(defn index-of [stack sym]
+  (.indexOf stack sym))
+
+(defn stack [initial]
+  (push-all! #js [] initial))
+
+(defn empty? [stack]
+  (= (length stack) 0))
+
+(defn not-empty? [stack]
+  (not (empty? stack)))
+
+(defn as-set [stack]
+  (into #{} stack))
+
+(defn as-vector [stack]
+  (into [] stack))
