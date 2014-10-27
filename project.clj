581d9dc2d47350a7d91b47bc51806ee7e1bb6408
Anders Hovmöller <boxed@killingar.net>
2014-10-27 18:27:32 +0100

    Updated version and contributors

--- project.clj
+++ project.clj
@@ -1,4 +1,4 @@
-(defproject instar "1.0.7"
+(defproject instar "1.0.8"
   :description "Simpler and more powerful assoc/dissoc/update-in"
   :dependencies [[org.clojure/clojure "1.6.0"]
                  [org.clojure/clojurescript "0.0-2371"]]
